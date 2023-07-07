e7b994b2f3942ac215f88c13ee422c87abc72736
quanming <qmtan17@gmail.com>
2023-07-07 14:50:27 +0800

    init

---
+++ Crafts.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Crafts.Website.Models;
+using Crafts.Website.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Crafts.Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet() //on get handles GET request
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
